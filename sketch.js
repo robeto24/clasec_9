981592570a8524b0a69a5d9da844d51c1c2acf3a
robeto24 <91990282+robeto24@users.noreply.github.com>
2021-10-05 16:18:02 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+
+var box;
+
+
+
+function setup() {
+  createCanvas(400,400);
+  box=createSprite(200,200,50,50);
+
+
+
+
+
+}
+
+function draw() 
+{
+  background(45);
+
+
+  drawSprites();
+
+}
+
+
+
+
